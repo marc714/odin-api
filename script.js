0b917e9d5882c2eb34f8fd143e12eba4f1c6a9f6
Marc Vo <vo.marc@gmail.com>
2023-01-23 16:10:44 -0800

    no change on main

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // https://www.theodinproject.com/lessons/node-path-javascript-asynchronous-code
 
 const img = document.querySelector('img');
-const imageButton = document.querySelector('#btn-image');
+const newImage = document.querySelector('#btn-image');
 const searchBtn = document.querySelector("#search-btn");
-imageButton.addEventListener('click', imageDOM);
+newImage.addEventListener('click', getImage);
 searchBtn.addEventListener('click', searchGif);
 
-// function ONLY gets image from API
 function getImage(){
   fetch('https://api.giphy.com/v1/gifs/translate?api_key=SApUDah5aiTL85ZBVexepgsZazATwhAQ&s=cats', {mode: 'cors'})
   .then(function(response) {
@@ -18,9 +17,7 @@ function getImage(){
   .then( response => {
   //console.log(response); // shows the json data
   //console.log(response.data.images.original.url) // shows the URL
-  
-  //img.src = response.data.images.original.url;
-  return response;
+  img.src = response.data.images.original.url;
   });
   // .then( response => img.src = response.data.images.original.url)
 };
@@ -39,11 +36,6 @@ function searchGif(e){
   .finally(console.log('search done'))
 }
 
-// Function only changes/updates image. Adding this for SOLID principles.
-async function imageDOM(){
-  let response = await getImage(); // you could make a global image variable to decouple this.
-  img.src = await response;
-}
 
 const onLoad = (() => {
   getImage();
